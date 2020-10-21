47d1f372e22a3783a54877f75d11cb1a01ac52dc
ofelman <61465999+ofelman@users.noreply.github.com>
2020-10-21 08:23:40 -0400

    Add files via upload

--- HPIARepo_ini.ps1
+++ HPIARepo_ini.ps1
@@ -2,7 +2,7 @@
     File: HPIARepo_ini.ps1
 
     Dan Felman/HP Inc
-    9/25/2020
+    10/20/2020
 
     modify variables as need for use by the Downloader script
     Separated script variables that can ne mod by Downloader
@@ -30,34 +30,32 @@ $NBSet1KG7 = 'HP Hotkey Support' , 'HP Notifications', 'Presence', '107683'     
 $DTSet1 = 'HP Notifications'
 
 # example HP Models to be listed in the GUI
+# variable 'SqName' is optional in the model list
 
 $HPModelsTable = @(
 	@{ ProdCode = '805D'; Model = "HP ProDesk 600 G2";                                        }
+    @{ ProdCode = '83EE'; Model = "HP ProDesk 600 G4 SFF";                                    }
+	@{ ProdCode = '8593'; Model = "HP EliteDesk 800 G5 Desktop Mini PC";    SqName = $DTSet1  }
+    @{ ProdCode = '8549'; Model = "HP EliteBook 840 G6 Healthcare Edition"; SqName = $NBSet1  }
+    @{ ProdCode = '859F'; Model = "HP EliteOne 800 G5 All-in-One";          SqName = $DTSet1  }
     @{ ProdCode = '83B3'; Model = "HP ELITEBOOK 830 G5";                    SqName = $NBSet1  }
 	@{ ProdCode = '83B2'; Model = "HP ELITEBOOK 840 G5";                    SqName = $NBSet1  }
 	@{ ProdCode = '8549'; Model = "HP ELITEBOOK 840 G6";                    SqName = $NBSet2  }
 	@{ ProdCode = '8723'; Model = "HP ELITEBOOK 840 G7";                    SqName = $NBSetG7 }
 	@{ ProdCode = '8470'; Model = "HP ELITEBOOK X360 1040 G5";              SqName = $NBSet1  }
 	@{ ProdCode = '80D4'; Model = "HP ZBook Studio G3"                                        }	
-	@{ ProdCode = '83EE'; Model = "HP ProDesk 600 G4 SFF";                                    }
 	@{ ProdCode = '844F'; Model = "HP ZBook Studio x360 G5";                SqName = $NBSet1  }
     @{ ProdCode = '844F'; Model = "HP ZBook Studio G5";                                       }
-	@{ ProdCode = '8593'; Model = "HP EliteDesk 800 G5 Desktop Mini PC";    SqName = $DTSet1  }
-    @{ ProdCode = '8549'; Model = "HP EliteBook 840 G6 Healthcare Edition"; SqName = $NBSet1  }
-    @{ ProdCode = '859F'; Model = "HP EliteOne 800 G5 All-in-One";          SqName = $DTSet1  }
-    )
-
-$FileServerName = $Env:COMPUTERNAME 
-
-$HPIACMPackage = 'HPIA'             # Package Name for creating/maintaining in SCCM
+	)
 
 #-------------------------------------------------------------------
-# choose/edit a path from next 2 entries for repository locations
+# choose/edit a (remote or local) path from next 2 entries for repository locations
+
+$FileServerName = $Env:COMPUTERNAME 
 
 # next for use in multiple repositories/one per model
 $RepoShareMain = $RepoShareMain = "\\$($FileServerName)\share\softpaqs\HPIARepoHead"
 #$RepoShareMain = "C:\share\softpaqs\HPIARepoHead"
-
 
 # and this for use when selecting a single repository (use with Script version >= 1.15
 $RepoShareCommon = "\\$($FileServerName)\share\softpaqs\HPIACommonRepository"
@@ -69,13 +67,17 @@ $LogFile = "$PSScriptRoot\HPDriverPackDownload.log"                   # Default 
 # if there errors during startup, set this to $true, for additional info
 $DebugMode = $false                                                   # Setting managed in GUI (temporarily)
 
+$HPIACMPackage = 'HPIA'             # Package Name for creating/maintaining in SCCM
+
 ####################################################################################
 #
-# Settngs managed by Script GUI - these are the defaults read by script
+# Settings managed by Script GUI - these are the defaults read by the script
 #
 ####################################################################################
 
 $OSVER = "1909"                 
+
+$HPIAVersion = '5.0.2.3827'         # info pulled from Browse app in UI
 
 #-------------------------------------------------------------------
 # next setting makes the script work with Microsoft SCCM/MECM, if set to $true
@@ -85,16 +87,8 @@ $OSVER = "1909"
 $HPIAPath = '\\CM01\Share\Applications\SP107374 HPIA 5.0.2'            
 #$HPIAPath = "C:\Share\Applications\HPIA-4.5.8.1"
 
-$HPIAVersion = '5.0.2.3827'
-
-$UpdateCMPackages = $False                         
-$DistributeCMPackages = $False                 
-
-#-------------------------------------------------------------------
-# 7/31 NEW: setting to keep filters (script version 1.35)
-$KeepFilters = $False                                    
-
-#-------------------------------------------------------------------
-# 7/21 NEW: manage single repository folder, instead of individual per model
-$CommonRepo = $True
-
+$UpdateCMPackages = $False
+$DistributeCMPackages = $False
+$Continueon404 = $False             # 10/20 NEW: setting to keep filters (script version 1.70)
+$KeepFilters = $False               # 7/31 NEW: setting to keep filters (script version 1.35)
+$CommonRepo = $True                 # 7/21 NEW: manage single repository folder, instead of individual per model
