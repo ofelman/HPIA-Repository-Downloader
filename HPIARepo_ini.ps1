1dca0f8659726d6ca116cf810c0a040652205a02
ofelman <61465999+ofelman@users.noreply.github.com>
2021-02-19 15:52:23 -0500

    Add files via upload
    Version 1.75 adds ability to import and use an existing HPIA repository, and do a OS version check for the selected platforms

--- HPIARepo_ini.ps1
+++ HPIARepo_ini.ps1
@@ -3,6 +3,7 @@
 
     Dan Felman/HP Inc
     10/20/2020
+    2/19/2021 - Update
 
     modify variables as need for use by the Downloader script
     Separated script variables that can ne mod by Downloader
@@ -13,16 +14,17 @@ if ( Test-Path variable:OS) {
 } else {
     New-Variable -Name OS -value "Win10" -Option ReadOnly
 }
-$OSVALID = @("1809", "1903", "1909", "2004")
+$OSVALID = @("1909", "2004", "2009")  # add OS version entries as needed to this variable
 
-# these are the Categories to be selected by HP IA, as needed from the repository
+# these are the Categories to be selected by HP IA - added as grid columns
 $FilterCategories = @('Driver','BIOS', 'Firmware', 'Software')
 
 #-------------------------------------------------------------------
 # These are sample Softpaq lists that we will download for a model
 #
 # NEW 1.30 - added list of individual softpaq names to host in repository
-#
+# ***** EXAMPLES *****
+
 $NBSet1 = 'HP Collaboration Keyboard Software' , 'HP Hotkey Support' , 'System Default Settings for Windows 10' , 'HP Notifications'
 $NBSet2 = 'HP Hotkey Support' , 'System Default Settings for Windows 10' , 'HP Notifications'
 $NBSetG7 = 'HP Hotkey Support' , 'HP Notifications', '107683'                   # sp107683='HP Programmable Key (SA) - version 1.0.13'
@@ -31,26 +33,17 @@ $DTSet1 = 'HP Notifications'
 
 # example HP Models to be listed in the GUI
 # variable 'SqName' is optional in the model list
+# NEW: versoin 1.75 adds ability to modify this list from existing, imported, repository
 
 $HPModelsTable = @(
-	@{ ProdCode = '805D'; Model = "HP ProDesk 600 G2";                                        }
-    @{ ProdCode = '83EE'; Model = "HP ProDesk 600 G4 SFF";                                    }
-	@{ ProdCode = '8593'; Model = "HP EliteDesk 800 G5 Desktop Mini PC";    SqName = $DTSet1  }
-    @{ ProdCode = '8549'; Model = "HP EliteBook 840 G6 Healthcare Edition"; SqName = $NBSet1  }
-    @{ ProdCode = '859F'; Model = "HP EliteOne 800 G5 All-in-One";          SqName = $DTSet1  }
-    @{ ProdCode = '83B3'; Model = "HP ELITEBOOK 830 G5";                    SqName = $NBSet1  }
-	@{ ProdCode = '83B2'; Model = "HP ELITEBOOK 840 G5";                    SqName = $NBSet1  }
-	@{ ProdCode = '8549'; Model = "HP ELITEBOOK 840 G6";                    SqName = $NBSet2  }
-	@{ ProdCode = '8723'; Model = "HP ELITEBOOK 840 G7";                    SqName = $NBSetG7 }
-	@{ ProdCode = '8470'; Model = "HP ELITEBOOK X360 1040 G5";              SqName = $NBSet1  }
-	@{ ProdCode = '80D4'; Model = "HP ZBook Studio G3"                                        }	
-	@{ ProdCode = '844F'; Model = "HP ZBook Studio x360 G5";                SqName = $NBSet1  }
-    @{ ProdCode = '844F'; Model = "HP ZBook Studio G5";                                       }
+	@{ ProdCode = '83EE'; Model = 'HP ProDesk 600 G4 Small Form Factor PC' } 
+	@{ ProdCode = '83B2'; Model = 'HP EliteBook 846 G5 Notebook PC' } 
+	@{ ProdCode = '8723'; Model = 'HP ZBook Firefly 14 G7 Mobile Workstation' }
 	)
 
 #-------------------------------------------------------------------
 # choose/edit a (remote or local) path from next 2 entries for repository locations
-
+# ... examples show both a \\ share and a drive letter entries ...
 $FileServerName = $Env:COMPUTERNAME 
 
 # next for use in multiple repositories/one per model
@@ -58,16 +51,15 @@ $RepoShareMain = $RepoShareMain = "\\$($FileServerName)\share\softpaqs\HPIARepoH
 #$RepoShareMain = "C:\share\softpaqs\HPIARepoHead"
 
 # and this for use when selecting a single repository (use with Script version >= 1.15
-$RepoShareCommon = "\\$($FileServerName)\share\softpaqs\HPIACommonRepository"
-#$RepoShareCommon = "C:\share\softpaqs\HPIACommonRepository"
+#$RepoShareCommon = "\\$($FileServerName)\share\softpaqs\HPIACommonRepository"
+$RepoShareCommon = "C:\HPIACommonRepository - Copy"
 
 #-------------------------------------------------------------------
-$LogFile = "$PSScriptRoot\HPDriverPackDownload.log"                   # Default to location where script runs from
 
-# if there errors during startup, set this to $true, for additional info
-$DebugMode = $false                                                   # Setting managed in GUI (temporarily)
+$LogFile = "$PSScriptRoot\HPDriverPackDownload.log"      # Default to location where script runs from
 
-$HPIACMPackage = 'HPIA'             # Package Name for creating/maintaining in SCCM
+
+$DebugMode = $false              # This allows startup debug info to be displayed, prior to GUI form creation                 
 
 ####################################################################################
 #
@@ -75,20 +67,17 @@ $HPIACMPackage = 'HPIA'             # Package Name for creating/maintaining in S
 #
 ####################################################################################
 
-$OSVER = "1909"                 
-
-$HPIAVersion = '5.0.2.3827'         # info pulled from Browse app in UI
+$OSVER = "1909"                  # this is the default version to be used
 
 #-------------------------------------------------------------------
-# next setting makes the script work with Microsoft SCCM/MECM, if set to $true
-# it can be modified in the main script, as needed
-
-# Add ability to create/update HPIA package - chose path from below
+# Items related to HPIA AND SCCM//MECM connection
+#-------------------------------------------------------------------
+$HPIACMPackage = 'HPIA'             # Package Name for creating/maintaining in SCCM
+$HPIAVersion = '5.0.2.3827'         # info pulled from Browse app in UI
 $HPIAPath = '\\CM01\Share\Applications\SP107374 HPIA 5.0.2'            
-#$HPIAPath = "C:\Share\Applications\HPIA-4.5.8.1"
+#-------------------------------------------------------------------
 
-$UpdateCMPackages = $False
-$DistributeCMPackages = $False
-$Continueon404 = $True              # 10/20 NEW: setting to keep filters (script version 1.70)
+$UpdateCMPackages = $False          
+$Continueon404 = $false             # useful during CMSL sync process
 $KeepFilters = $False               # 7/31 NEW: setting to keep filters (script version 1.35)
-$CommonRepo = $True                 # 7/21 NEW: manage single repository folder, instead of individual per model
+$CommonRepo = $True
